fe810439ec9b1c2e5af92cef800b69cf999abc55
Carsten Jacobsen <car.jacobsen@gmail.com>
2024-02-04 14:15:55 +0100

    fix(tag): add missing icon

--- src/icons.js
+++ src/icons.js
@@ -3,5 +3,6 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash'
 import { faBan } from '@fortawesome/free-solid-svg-icons/faBan'
 import { faImage } from '@fortawesome/free-solid-svg-icons/faImage'
 import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
+import { faTag } from '@fortawesome/free-solid-svg-icons/faTag'
 
-library.add(faTrash, faBan, faImage, faPlus)
+library.add(faTrash, faBan, faImage, faPlus, faTag)
